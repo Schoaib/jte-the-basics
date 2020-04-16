1e772971745d931b0a1647bb019897bdc7d756a4
schoaib <s.shoaib@NBC02ZT586MD6T.EMIRATES.COM>
2020-04-16 16:40:05 +0400

    add pod template

--- libraries/utility/node.groovy
+++ libraries/utility/node.groovy
@@ -1,5 +1,10 @@
 void call(Closure body) {
-    steps.podTemplate(inheritFrom: config.label ?: 'dev-slave') {
+    steps.podTemplate(inheritFrom: config.label ?: 'dev-slave',   containers: [  containerTemplate(
+        name: 'docker', image: 'docker:latest',
+        resourceRequestCpu: '50m', resourceLimitCpu: '500m',
+        resourceRequestMemory: '200Mi', resourceLimitMemory: '500Mi',
+        ttyEnabled: true,command: 'cat'),
+  ]) {
         steps.node(POD_LABEL) {
             body()
         }
