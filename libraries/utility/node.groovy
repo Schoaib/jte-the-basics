73cf08091f54ce8e924214ca4a8d860576d61b18
schoaib <s.shoaib@NBC02ZT586MD6T.EMIRATES.COM>
2020-04-16 15:51:16 +0400

    add pod template

--- libraries/utility/node.groovy
+++ libraries/utility/node.groovy
@@ -1,5 +1,5 @@
 void call(Closure body) {
-    steps.podTemplate(containers: [ containerTemplate(
+    steps.podTemplate(inheritFrom: 'dev-slave',containers: [ containerTemplate(
         name: 'golang', image: 'golang:1.9.2-stretch',
         resourceRequestCpu: '50m', resourceLimitCpu: '500m',
         resourceRequestMemory: '200Mi', resourceLimitMemory: '500Mi',
