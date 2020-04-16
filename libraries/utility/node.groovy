c03e129ae7b3525379f6bfb17d626c8a096488ee
schoaib <s.shoaib@NBC02ZT586MD6T.EMIRATES.COM>
2020-04-16 15:45:42 +0400

    add pod template

--- libraries/utility/node.groovy
+++ libraries/utility/node.groovy
@@ -1,15 +1,11 @@
-void call(String label = null, Closure body){
-    podTemplate(containers: [
-    containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat'),
-    containerTemplate(name: 'golang', image: 'golang:1.8.0', ttyEnabled: true, command: 'cat'),
-     containerTemplate(
+void call(Closure body) {
+    steps.podTemplate(containers: [ containerTemplate(
         name: 'golang', image: 'golang:1.9.2-stretch',
         resourceRequestCpu: '50m', resourceLimitCpu: '500m',
         resourceRequestMemory: '200Mi', resourceLimitMemory: '500Mi',
-        ttyEnabled: true,command: 'cat')
-  ]) {
-   steps.node( label ?: config.label ?: “” ){
-    body()
-  }
-  }
+        ttyEnabled: true,command: 'cat') ]) {
+        steps.node(POD_LABEL) {
+            body()
+        }
+    }
 }
